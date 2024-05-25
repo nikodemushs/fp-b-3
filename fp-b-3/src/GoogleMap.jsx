032185c88c2b1b4a14edec344bd944f710a67e4e
nikodemushs <nikodemushs@gmail.com>
2024-05-25 14:03:02 +0700

    init component

---
+++ fp-b-3/src/GoogleMap.jsx
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+import GoogleMapReact from "google-map-react";
+
+const GoogleMaps = ({ latitude, longitude }) => {
+  const renderMarkers = (map, maps) => {
+    let marker = new maps.Marker({
+      position: { lat: latitude, lng: longitude },
+      map,
+      title: "Hello World!",
+    });
+    return marker;
+  };
+
+  return (
+    <Fragment>
+      <div style={{ height: "300px", width: "400px" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAOCgj-boqDL7xGJCRANYAZasOa9bY96AM" }}
+          defaultCenter={{ lat: -6.125556, lng: 106.65583 }}
+          defaultZoom={16}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+        ></GoogleMapReact>
+      </div>
+    </Fragment>
+  );
+};
+
+export default GoogleMaps;
